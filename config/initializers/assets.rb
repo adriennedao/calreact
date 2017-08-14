50ba848cd987ac355d627d625b596661bbbd26a4
AdrienneD <adriennedao@gmail.com>
2017-08-14 12:46:21 -0400

    Hot module replacement

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -19,3 +19,9 @@ Rails.application.config.assets.version = '1.0'
 
 # Add folder with webpack generated assets to assets.paths
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "webpack")
+
+type = ENV["REACT_ON_RAILS_ENV"] == "HOT" ? "non_webpack" : "static"
+Rails.application.config.assets.precompile +=
+  [
+    "application_#{type}.js"
+  ]
